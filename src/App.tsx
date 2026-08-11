e7cd538c8a0cbfa7f79f81b29693fa10a03737ba
Yliubov <129996599+YLiubov@users.noreply.github.com>
2026-08-11 10:21:50 +0200

    update container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
+import { Container } from "./components/Container";
 
 const App = () => {
 
@@ -8,7 +9,25 @@ const App = () => {
     <>
       <Header />
 
-      <Main />
+      <Main>
+            <Container
+        width="100%"
+        backgroundColor="#29cbe8"
+        padding="40px"
+      >
+        <h1>FN's Verdensmål</h1>
+        <p>De 17 verdensmål for bæredygtig udvikling.</p>
+      </Container>
+
+      <Container
+        width="80%"
+        backgroundColor="#b8dc42"
+        padding="30px"
+      >
+        <h2>Verdensmål 17</h2>
+        <p>Partnerskaber for handling.</p>
+      </Container>
+      </Main>
 
       <Footer />
     </>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,25 +1,11 @@
-import { Container } from "./Container";
+type MainProps = {
+  children: React.ReactNode;
+};
 
-export const Main = () => {
+export const Main = ({ children }: MainProps) => {
   return (
     <main>
-      <Container
-        width="100%"
-        backgroundColor="#29cbe8"
-        padding="40px"
-      >
-        <h1>FN's Verdensmål</h1>
-        <p>De 17 verdensmål for bæredygtig udvikling.</p>
-      </Container>
-
-      <Container
-        width="80%"
-        backgroundColor="#b8dc42"
-        padding="30px"
-      >
-        <h2>Verdensmål 17</h2>
-        <p>Partnerskaber for handling.</p>
-      </Container>
+      {children}
     </main>
   );
 };
